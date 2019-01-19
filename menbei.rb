cb43bb9d1fb8a88985ef23b4b857df6e5b9a4fe2
nasa <htilcs1115@gmail.com>
2019-01-19 19:51:55 +0900

    Add formular

---
+++ menbei.rb
@@ -0,0 +1,14 @@
+class Menbei < Formula
+  desc 'Tool to generate github issue link'
+  homepage 'https://github.com/k-nasa/menbei'
+  url 'https://github.com/k-nasa/menbei/releases/download/v0.1.0/menbeiv0.1.1-x86_64-apple-darwin.zip'
+  version '0.1.0'
+
+  def install
+    bin.install 'menbei'
+  end
+
+  test do
+    system "#{bin}/menbei --version"
+  end
+end

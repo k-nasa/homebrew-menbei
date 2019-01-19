959283847da8062f669a6e9c9e011defd8c9c0e5
nasa <htilcs1115@gmail.com>
2019-01-19 20:03:01 +0900

    fix download link

--- menbei.rb
+++ menbei.rb
@@ -1,7 +1,7 @@
 class Menbei < Formula
   desc 'Tool to generate github issue link'
   homepage 'https://github.com/k-nasa/menbei'
-  url 'https://github.com/k-nasa/menbei/releases/download/v0.1.0/menbeiv0.1.1-x86_64-apple-darwin.zip'
+  url 'https://github.com/k-nasa/menbei/releases/download/v0.1.0/menbeiv0.1.0-x86_64-apple-darwin.zip'
   version '0.1.0'
 
   def install
